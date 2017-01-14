60010a0ec03fac979962d987049905fd376e9e22
Mina Asham <mina.asham@hotmail.com>
2017-01-14 12:48:26 +0000

    Add project structure

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='pictures-dedupe-and-rename',
+    version='1.0.0',
+    description='Dedupe a set of pictures in a given folder and rename them using the yyyymmdd_HHMMss format',
+    url='https://github.com/mina-asham/pictures-dedupe-and-rename',
+    license='MIT',
+    author='Mina Asham',
+    author_email='mina.asham@hotmail.com',
+    packages=find_packages(),
+    install_requires=[],
+    long_description=open('README.md').read()
+)
