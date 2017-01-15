b5349e39298bf60424c60205f2ba375e33f78c54
Mina Asham <mina.asham@hotmail.com>
2017-01-15 16:52:27 +0000

    Bugfix: Don't dedupe against already deleted files

--- pictures/dedupe/dedupe.py
+++ pictures/dedupe/dedupe.py
@@ -8,8 +8,10 @@ logger = logger_from(__name__)
 
 
 def dedupe(filenames, prefer_shorter=True):
+    deleted = set()
     for filename1, filename2 in itertools.combinations(filenames, 2):
-        if filecmp.cmp(filename1, filename2):
+        if filename1 not in deleted and filename2 not in deleted and filecmp.cmp(filename1, filename2):
             file_to_delete = max(filename1, filename2) if prefer_shorter else min(filename1, filename2)
             os.remove(file_to_delete)
+            deleted.add(file_to_delete)
             logger.info('Deleted: %s', file_to_delete)
--- tests/test_dedupe.py
+++ tests/test_dedupe.py
@@ -1,4 +1,3 @@
-import itertools
 from unittest import TestCase
 from unittest import mock
 from unittest.mock import MagicMock
@@ -12,20 +11,38 @@ def create_mock_cmp(equal_files):
 
 
 class TestDedupe(TestCase):
-    EQUAL_FILES = ['filename1', 'filename2']
-    NON_EQUAL_FILES = ['filename3', 'filename4']
+    EQUAL_FILES = ['filename1', 'filename2', 'filename3']
+    NON_EQUAL_FILES = ['filename4', 'filename5']
     FILES = EQUAL_FILES + NON_EQUAL_FILES
 
     @mock.patch('os.remove')
     @mock.patch('filecmp.cmp', side_effect=create_mock_cmp(EQUAL_FILES))
     def test_dedupe_prefer_shorter(self, mock_cmp: MagicMock, mock_remove: MagicMock):
         dedupe(self.FILES)
-        self.assertEquals(mock_cmp.mock_calls, test_helpers.calls_from(itertools.combinations(self.FILES, 2)))
-        mock_remove.assert_called_once_with('filename2')
+        self.assertEquals(mock_cmp.mock_calls, test_helpers.calls_from([
+            ('filename1', 'filename2'),
+            ('filename1', 'filename3'),
+            ('filename1', 'filename4'),
+            ('filename1', 'filename5'),
+            ('filename4', 'filename5')
+        ]))
+        self.assertEquals(mock_remove.mock_calls, test_helpers.calls_from([
+            ('filename2',),
+            ('filename3',)
+        ]))
 
     @mock.patch('os.remove')
     @mock.patch('filecmp.cmp', side_effect=create_mock_cmp(EQUAL_FILES))
     def test_dedupe_prefer_longer(self, mock_cmp: MagicMock, mock_remove: MagicMock):
         dedupe(self.FILES, prefer_shorter=False)
-        self.assertEquals(mock_cmp.mock_calls, test_helpers.calls_from(itertools.combinations(self.FILES, 2)))
-        mock_remove.assert_called_once_with('filename1')
+        self.assertEquals(mock_cmp.mock_calls, test_helpers.calls_from([
+            ('filename1', 'filename2'),
+            ('filename2', 'filename3'),
+            ('filename3', 'filename4'),
+            ('filename3', 'filename5'),
+            ('filename4', 'filename5')
+        ]))
+        self.assertEquals(mock_remove.mock_calls, test_helpers.calls_from([
+            ('filename1',),
+            ('filename2',)
+        ]))
