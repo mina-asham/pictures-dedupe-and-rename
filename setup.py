173f28cc145fcf16982e7f241602804ec2d95c53
Mina Asham <mina.asham@hotmail.com>
2017-01-16 00:49:35 +0000

    Remove mock from setup_requires and update Travis configuration to use test_requirements.txt

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PicturesDedupeRename',
-    version='1.0.3',
+    version='1.0.4',
     description='Dedupe a set of pictures in a given folder and rename them using the yyyymmdd_HHMMss format',
     url='https://github.com/mina-asham/pictures-dedupe-and-rename',
     license='MIT',
@@ -11,6 +11,6 @@ setup(
     author_email='mina.asham@hotmail.com',
     scripts=['dedupe-rename.py'],
     packages=find_packages(),
-    install_requires=['mock', 'exifread'],
+    install_requires=['exifread'],
     long_description=open('README.rst').read()
 )
